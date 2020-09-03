49a871760d01cb385fc96fd55b95d430b49ccb26
Deepak Mettem <dmettem@vmware.com>
2020-09-02 20:02:26 -0700

    Add dollar filter query param to flavor and image profile APIs (#25)
    Signed-off-by: Deepak Mettem <dmettem@vmware.com>

--- pkg/client/flavor_profile/get_flavor_profiles_parameters.go
+++ pkg/client/flavor_profile/get_flavor_profiles_parameters.go
@@ -61,6 +61,11 @@ for the get flavor profiles operation typically these are written to a http.Requ
 */
 type GetFlavorProfilesParams struct {
 
+	/*DollarFilter
+	  Add a filter to return limited results
+
+	*/
+	DollarFilter *string
 	/*APIVersion
 	  The version of the API in yyyy-MM-dd format (UTC). For versioning information refer to /iaas/api/about
 
@@ -105,6 +110,17 @@ func (o *GetFlavorProfilesParams) SetHTTPClient(client *http.Client) {
 	o.HTTPClient = client
 }
 
+// WithDollarFilter adds the dollarFilter to the get flavor profiles params
+func (o *GetFlavorProfilesParams) WithDollarFilter(dollarFilter *string) *GetFlavorProfilesParams {
+	o.SetDollarFilter(dollarFilter)
+	return o
+}
+
+// SetDollarFilter adds the dollarFilter to the get flavor profiles params
+func (o *GetFlavorProfilesParams) SetDollarFilter(dollarFilter *string) {
+	o.DollarFilter = dollarFilter
+}
+
 // WithAPIVersion adds the aPIVersion to the get flavor profiles params
 func (o *GetFlavorProfilesParams) WithAPIVersion(aPIVersion *string) *GetFlavorProfilesParams {
 	o.SetAPIVersion(aPIVersion)
@@ -123,6 +139,22 @@ func (o *GetFlavorProfilesParams) WriteToRequest(r runtime.ClientRequest, reg st
 		return err
 	}
 	var res []error
+
+	if o.DollarFilter != nil {
+
+		// query param $filter
+		var qrDollarFilter string
+		if o.DollarFilter != nil {
+			qrDollarFilter = *o.DollarFilter
+		}
+		qDollarFilter := qrDollarFilter
+		if qDollarFilter != "" {
+			if err := r.SetQueryParam("$filter", qDollarFilter); err != nil {
+				return err
+			}
+		}
+
+	}
 
 	if o.APIVersion != nil {
 
