8833908f64667906756bb83ab4481ebd4055a66e
Ferran Rodenas <ferran.rodenas@broadcom.com>
2025-04-23 13:47:20 -0700

    Fix Policy API (#50)
    * Add TypeID to Policy GET operation
    * Renames Policy POST operation
    
    Signed-off-by: Ferran Rodenas <ferran.rodenas@broadcom.com>

--- pkg/client/policies/policies_client.go
+++ pkg/client/policies/policies_client.go
@@ -30,15 +30,56 @@ type ClientOption func(*runtime.ClientOperation)
 
 // ClientService is the interface for Client methods
 type ClientService interface {
-	DeletePolicyUsingDELETE5(params *DeletePolicyUsingDELETE5Params, opts ...ClientOption) (*DeletePolicyUsingDELETE5NoContent, error)
+	CreatePolicyUsingPOST1(params *CreatePolicyUsingPOST1Params, opts ...ClientOption) (*CreatePolicyUsingPOST1OK, *CreatePolicyUsingPOST1Created, error)
 
-	DryRunPolicyUsingPOST2(params *DryRunPolicyUsingPOST2Params, opts ...ClientOption) (*DryRunPolicyUsingPOST2OK, *DryRunPolicyUsingPOST2Created, error)
+	DeletePolicyUsingDELETE5(params *DeletePolicyUsingDELETE5Params, opts ...ClientOption) (*DeletePolicyUsingDELETE5NoContent, error)
 
 	GetPoliciesUsingGET5(params *GetPoliciesUsingGET5Params, opts ...ClientOption) (*GetPoliciesUsingGET5OK, error)
 
 	GetPolicyUsingGET5(params *GetPolicyUsingGET5Params, opts ...ClientOption) (*GetPolicyUsingGET5OK, error)
 
 	SetTransport(transport runtime.ClientTransport)
+}
+
+/*
+CreatePolicyUsingPOST1 triggers a policy dry run
+
+Dry-run an existing policy to rehearse actual policy effect on application.
+*/
+func (a *Client) CreatePolicyUsingPOST1(params *CreatePolicyUsingPOST1Params, opts ...ClientOption) (*CreatePolicyUsingPOST1OK, *CreatePolicyUsingPOST1Created, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewCreatePolicyUsingPOST1Params()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "createPolicyUsingPOST_1",
+		Method:             "POST",
+		PathPattern:        "/policy/api/policies",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"https"},
+		Params:             params,
+		Reader:             &CreatePolicyUsingPOST1Reader{formats: a.formats},
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, nil, err
+	}
+	switch value := result.(type) {
+	case *CreatePolicyUsingPOST1OK:
+		return value, nil, nil
+	case *CreatePolicyUsingPOST1Created:
+		return nil, value, nil
+	}
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for policies: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
 }
 
 /*
@@ -78,47 +119,6 @@ func (a *Client) DeletePolicyUsingDELETE5(params *DeletePolicyUsingDELETE5Params
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for deletePolicyUsingDELETE_5: API contract not enforced by server. Client expected to get an error, but got: %T", result)
-	panic(msg)
-}
-
-/*
-DryRunPolicyUsingPOST2 triggers a policy dry run
-
-Dry-run an existing policy to rehearse actual policy effect on application.
-*/
-func (a *Client) DryRunPolicyUsingPOST2(params *DryRunPolicyUsingPOST2Params, opts ...ClientOption) (*DryRunPolicyUsingPOST2OK, *DryRunPolicyUsingPOST2Created, error) {
-	// TODO: Validate the params before sending
-	if params == nil {
-		params = NewDryRunPolicyUsingPOST2Params()
-	}
-	op := &runtime.ClientOperation{
-		ID:                 "dryRunPolicyUsingPOST_2",
-		Method:             "POST",
-		PathPattern:        "/policy/api/policies",
-		ProducesMediaTypes: []string{"application/json"},
-		ConsumesMediaTypes: []string{"application/json"},
-		Schemes:            []string{"https"},
-		Params:             params,
-		Reader:             &DryRunPolicyUsingPOST2Reader{formats: a.formats},
-		Context:            params.Context,
-		Client:             params.HTTPClient,
-	}
-	for _, opt := range opts {
-		opt(op)
-	}
-
-	result, err := a.transport.Submit(op)
-	if err != nil {
-		return nil, nil, err
-	}
-	switch value := result.(type) {
-	case *DryRunPolicyUsingPOST2OK:
-		return value, nil, nil
-	case *DryRunPolicyUsingPOST2Created:
-		return nil, value, nil
-	}
-	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
-	msg := fmt.Sprintf("unexpected success response for policies: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
